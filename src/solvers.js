e370580e3417dd6b7e530e83e0aaeecf00317167
Hieu Ngo <hieu.ngo12989@gmail.com>
2022-09-16 10:20:32 -0700

    Completed n-queens w/ Partner Monica Liu

--- src/solvers.js
+++ src/solvers.js
@@ -63,62 +63,28 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  if (n === 2 || n === 3) {
-    return n;
+  if (n === 0) {
+    return [];
   }
-
   let myBoard = new Board({n: n});
-  let allSolutions = [];
-  console.log(myBoard.rows())
-  let solution;
-
-
-  var check = function (x){
-    if (x === n) {
-      // console.log('current solution: ', solution);
-      return;
+  var check = function (row) { // x = rowIndex
+    if (row === n) {
+      return true;
     }
-
-    for ( var i = 0; i < n; i++) {
-      myBoard.togglePiece(x, i)
+    for (var col = 0; col < n; col++) {
+      myBoard.togglePiece(row, col);
       if (!myBoard.hasAnyQueensConflicts()) {
-        check(x + 1)
-      } else {
-        myBoard.togglePiece(x, i)
+        if (check(row + 1)) {
+          return true;
+        }
       }
-      if (i + 1 === n) {
-        check(x + 1);
-      }
-      check(x + 1);
-      // return;
+      myBoard.togglePiece(row, col);
     }
-  }
+  };
   check(0);
-
-
-
-  // var check = function (x){
-  //   solution = myBoard.rows();
-  //   if (x === n) {
-  //     solution = myBoard.rows();
-  //     return;
-  //   }
-
-  //   for ( var i = 0; i < n; i++) { // x = 0, i = 0
-  //     myBoard.togglePiece(x, i)
-  //     if (!myBoard.hasAnyQueensConflicts()) {
-  //       check(x + 1)
-  //     } else {
-  //       myBoard.togglePiece(x, i)
-  //     }
-  //   }
-  // }
-  // check(0)
-
-
-  // console.log('allsolutions: ', allSolutions)
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return myBoard.rows();
+  let solution = myBoard.rows();
+    console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
+  return solution;
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
